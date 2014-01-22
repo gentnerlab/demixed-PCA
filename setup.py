756b5721115b2ed5d3410c2118f3c0763be4f940
Justin Kiggins <justin.kiggins@gmail.com>
2014-01-21 22:54:42 -0800

    added for distutils

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='demixed-PCA',
+    version='0.1dev',
+    packages=['dPCA',],
+    license=open('LICENSE').read(),
+    long_description=open('README.md').read(),
+)
